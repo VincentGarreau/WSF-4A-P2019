eb2cd015c12ce6e48a28420d32fa00784f0c3962
Vincent Garreau <vin.garreau@gmail.com>
2018-03-27 09:04:18 +0200

    add vue-starter

---
+++ vue-starter/src/App.vue
@@ -0,0 +1,11 @@
+<!-- HTML -->
+<template>
+  <span class="hello">Hello World!</span>
+</template>
+
+<!-- Javascript -->
+<script>
+</script>
+
+<!-- Style -->
+<style lang="stylus" src="./App.styl"/>
